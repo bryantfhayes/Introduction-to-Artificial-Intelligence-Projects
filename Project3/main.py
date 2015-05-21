9930e1a84d737ec3840fea12b42fa243c22d29af
Bryant Hayes <BryantfHayes@gmail.com>
2015-05-21 00:10:51 -0700

    Part 1 of assignment is complete. Preprocessor phase done

--- Project3/main.py
+++ Project3/main.py
@@ -1,50 +1,108 @@
-import sys, os, random
+import sys, os, random, re
 
 # -------------------------
 # BEGIN PREPROCESSING PHASE
 # -------------------------
 
+class Vocabulary():
+    def __init__(self):
+        self.commonWords = []
+        self._words = {}
+
+    def put(self, word):
+        if word in self._words:
+            self._words[word] += 1
+        else:
+            self._words[word] = 1
+
+    def get(self, word):
+        if word in self._words:
+            return self._words[word]
+        else:
+            return 0
+
+    def contains(self, word):
+        if word in self._words:
+            return 1
+        else:
+            return 0
+
+    def buildArray(self):
+        for key, val in self._words.iteritems():
+            if val > 5:
+                self.commonWords.append(key)
+        self.commonWords = sorted(self.commonWords)
+
 # Read each line of a file into a 2D array containing sentence and class label
 # PARAMS: Name of the file to read. Must be in working directory.
 # RETURN: 2d-Array containing sentence[0] and  classlabel[1]
 def readRawFile(filename):
-    pass
+    sentences = []
+    with open(filename) as f:
+        content = f.readlines()
+    for line in content:
+        raw = re.findall('(".*?",)|(?<=,)([0-1])', line)
+        sentences.append([raw[0][0].lower(), raw[1][1]])
+    return sentences
 
 # Remove all apostrophes from each sentence in an array of sentences.
 # PARAMS: Array of strings to clean
 # RETURN: Array of original strings minus apostrophes
-def removeApostrophes(sentences):
-    pass
+def removeSymbols(sentences):
+    for sentence in sentences:
+        sentence[0] = re.sub("[']", '', sentence[0])
+        sentence[0] = re.sub("[^\w]", ' ', sentence[0])
+    return sentences
 
 # Takes a list of sentence strings and returns an array of seperate words
 # PARAMS: Sentences to break up
 # RETURN: Array of list of words
 def breakupSentence(sentences):
-    pass
+    for sentence in sentences:
+        sentence[0] = re.findall("([^\s]+)", sentence[0])
+    return sentences
 
 # Takes an array of word lists and forms a vocabularity of words seen 5 or more times.
 # PARAMS: Array of word lists to build vocab.
 # RETURN: A one dimensional list of vocab words in alphabetical order.
-def createVocabulary(sentences):
-    pass
+def createVocabulary(wordlists):
+    vocab = Vocabulary()
+    for wordlist in wordlists:
+        for word in wordlist[0]:
+            vocab.put(word)
+    vocab.buildArray()
+    return vocab
 
 # Taking a vocab list and a list of words from a sentence this function creates
 # a list of length [len(vocab)+1] which contains an index for each word from the vocab
 # set. Each index is either 0 (sentence doesn't contain word) or 1 (sentence contains word).
 # The last index is reserved for classlabel
 # ex) 1,0,1,0, ... ,1
-# PARAMS: list of vocab words, a single sentence's wordlist and classlabel (2d array)
-# RETURN: Single feature in the form of (0,1,0,0,1,<classlabel>)
-def createFeature(vocab, sentence):
-    pass
+# PARAMS: [wordlist, classLabel] list
+# RETURN: Single feature in the form of [0,1,0,0,1,<classlabel>]
+def createFeature(vocab, wordlist):
+    feature = []
+    for word in vocab.commonWords:
+        if word in wordlist[0]:
+            feature.append(1)
+        else:
+            feature.append(0)
+    feature.append(int(wordlist[1]))
+    return feature
 
 # Save the designated pre-processed data to a file. The format of the file is:
 # List of vocab words, ending in 'classlabel'
 # List of featurized sentences seperated by commas
 # PARAMS: list of sentences in feature form, output filename
 # RETURN: None
-def savePreprocessedData(sentences, filename):
-    pass
+def savePreprocessedData(vocab, features, filename):
+    f = open(filename, 'w')
+    [f.write(word + ",") for word in vocab.commonWords]
+    f.write("classlabel\n")
+    for feature in features:
+        f.write(str(feature)[1:-1])
+        f.write('\n')
+    f.close()
 
 # -----------------------
 # END PREPROCCESING PHASE
@@ -103,7 +161,28 @@ def analyzeResults():
 # ------------------------
 
 def main():
-  print "Hello World"
+    # Get raw file data
+    trainingData = readRawFile("training_text.txt")
+    testData = readRawFile("test_text.txt")
+
+    # Remove symbols
+    trainingData = removeSymbols(trainingData)
+    testData = removeSymbols(testData)
+
+    # Breakup into word lists
+    trainingData = breakupSentence(trainingData)
+    testData = breakupSentence(testData)
+
+    # Create alphabetized array of common words
+    vocab = createVocabulary(trainingData + testData)
+
+    # Create features for each wordlist
+    trainingFeatures = [createFeature(vocab, wordlist) for wordlist in trainingData]
+    testFeatures = [createFeature(vocab, wordlist) for wordlist in testData]
+
+    # Save each feature list to a file
+    savePreprocessedData(vocab, trainingFeatures, "preprocessed_train.txt")
+    savePreprocessedData(vocab, testFeatures, "preprocessed_test.txt")
 
 if __name__ == "__main__":
-  main()
+    main()
