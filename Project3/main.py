774290756ef9f216032d029b4240d792bdd8aa9d
Bryant Hayes <BryantfHayes@gmail.com>
2015-05-20 18:56:41 -0700

    Expanded skeleton code

--- Project3/main.py
+++ Project3/main.py
@@ -54,9 +54,44 @@ def savePreprocessedData(sentences, filename):
 # BEGIN CLASSIFICATION PHASE
 # --------------------------
 
+# Store each vocab word as a key in a large dictionary.
+# Each dictionaries corresponding value will be an 4x3 array encapsulated by a truthtable calss
+# F F - [0][0] = P(%)
+# F T - [0][1] = P(%)
+# T F - [1][0] = P(%)
+# T T - [1][1] = P(%)
+#
+# Truthtable class with be able to GETPROB(0, 1) and return the corresponding probability.
+# SETPROB(0, 0) = 0.02 sets probability.
+# Implemented with internal 2d array
+# _table variable containing zeroes to start
+# INIT(name, vocab index, trainingdata):
+#   loop through test data incrementing ff, ft, tf, tt, clt <classlabel true>, clf <classlabel false> via large if-elif
+#   after one loop all data needed to calculate P(word | classlabel) table will be obtained
+#   CreateTable(ff, ft, tf, tt, clt, clf)
+#
+# _name = "word from vocab"
+#
+# A function will formulate all of these tables by iterating through every vocab word
+# and determining probabilities.
+#
+# With this we just pass in the truth value for BD as well as
+# which vocab word and the value for the vocab word (0 or 1). we want to know the
+# probabiltiy for ie) P(m|BD) and the dictionary is accessed
+# via the key and then depending on whether m=true/false and bd=true/false the percentage is returned.
+#
+# These percentages are multiplied together to find P(bd | m, a, g, ...) as well as P(!bd | m, a, g, ...)
+
+# These two results are compared and which ever has higher percentage then that is the guess.
+
+# Formulate dictionary of vocab words with their associated percentages in the form of a truth table
+# PARAMS: Vocab list, training data set in featurized form
+# RETURN: Dictionary containing all probabilities P(word1 | sarcastic), P(word2 | sarcastic), ... etc
 def train():
     pass
 
+# Looks at the vocab dictionary given the current featurized list. Tries both sarcastic=1 and sarcastic=0.
+# Finds the product of each class label and chooses the most probable one.
 def classify():
     pass
 
